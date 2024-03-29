4e6019d9b2bdbc4d423ae12bdda532b5b8f040d1
Meher Askri <meheraskri@gmx.com>
2024-03-29 04:12:38 +0100

    apache_project

--- repo.sh
+++ repo.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-# Check CD device availability
 if [ -e "/dev/sr0" ] ; then
-    	mount /dev/sr0 /mnt && cd /mnt && mkdir /repo # mount the CD and create a Directory
-    	tar cvf - . | (cd /repo/; tar xvf -)  # Copy media content to the local directory by 
+    	mount /dev/sr0 /mnt && cd /mnt && mkdir /repo # mount CD and create dir
+    	tar cvf - . | (cd /repo/; tar xvf -)  # Copy media content to local dir
 else
 	echo " Insert a CD  and try again"
 fi
-# Removing any existing config files and create new config file
+# Remove existing config && create new config file
 rm -rf /etc/yum.repos.d/* && touch /etc/yum.repos.d/local.repo
-# Configure the local YUM/DNF repository
+# Configure the new repo
 cat >> /etc/yum.repos.d/local.repo << EOF
 [LocalRepo_AppStream]
 name=LocalRepo_AppStream
@@ -23,27 +22,25 @@ gpgcheck=1
 baseurl=file:///repo/BaseOS/
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rockyofficial
 EOF
-# Install Apache with mod_ssl and createrepo package
 dnf update && dnf install createrepo httpd mod_ssl  -y
-# creating the repo directory and copy the official gpg key and start Apache  
 createrepo /repo/ && cp /etc/pki/rpm-gpg/RPM-GPG-KEY-rockyofficial /repo 
 systemctl enable --now httpd
-# opening the http and https ports on firewall
+# Open ports in firewalld
 for i in http https ; do
         firewall-cmd --add-service=$i --permanent 2>/dev/null && firewall-cmd --reload
 done
-# Using sed to modify the DocumentRoot and the Directory in Apache config file && delete the default welcome page
+# Config apache
 sed -i 's/^DocumentRoot.*/DocumentRoot "\/repo"/' /etc/httpd/conf/httpd.conf
 sed -i 's/^<Directory "\/var\/www\/html"/<Directory "\/repo"/' /etc/httpd/conf/httpd.conf
 rm -rf /etc/httpd/conf.d/welcome*
-# Create an ACL to give Apache full access && setting the Selinux context type on the repo directory 
+# Create ACL && set Selinux context 
 setfacl -R -m u:apache:rwx  /repo && semanage fcontext -m -t httpd_sys_content_t "/repo(/.*)?" && restorecon -Rv /repo
-# generate a self signed certifiacte and a private key for the TLS encryption
+# generate SSL cert
 openssl req -x509 -nodes -days 365 --newkey rsa:2048 -keyout /etc/pki/tls/private/myrepo.key -out /etc/pki/tls/certs/myrepo.crt -subj "/CN=$HOSTNAME"
-# Modify the SSL config file with sed to the path of the certificate and the key 
+# Modify SSL config file 
 sed -i 's/^SSLCertificateFile.*/SSLCertificateFile \/etc\/pki\/tls\/certs\/myrepo.crt/' /etc/httpd/conf.d/ssl.conf
 sed -i 's/^SSLCertificateKeyFile.*/SSLCertificateKeyFile \/etc\/pki\/tls\/private\/myrepo.key/' /etc/httpd/conf.d/ssl.conf
-# create a config file in the Apache to redirect http traffic to https
+# Redirect http traffic to https
 touch /etc/httpd/conf.d/myrepo.conf
 cat >> /etc/httpd/conf.d/myrepo.conf << SIG
 <VirtualHost *:80>
@@ -51,5 +48,5 @@ ServerName 10.1.1.9
 Redirect "/" "https://10.1.1.9/"
 </VirtualHost>
 SIG
-# Restart the Apache service and unmount the CD device
+# Restart Apache && Unmount CD device
 systemctl restart httpd && umount /mnt
